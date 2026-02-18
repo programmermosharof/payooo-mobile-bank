ce517b5145d70f6b3986e00fba7e391f89eb902d
programmermosharof <mosharof.dev@gmail.com>
2026-02-18 17:57:38 +0600

    Cashout Page complete

--- JavaScript/cashout.js
+++ JavaScript/cashout.js
@@ -1,3 +1,48 @@
 document.getElementById('cashout-btn').addEventListener('click', function(){
+
+    // 1: ============== Get The Agent Number $ Validate ==============
+    const cashoutNumberInput = document.getElementById("cashout-number");
+    const cashoutNumber = cashoutNumberInput.value;
+    console.log(cashoutNumber);
+
+    if(cashoutNumber.length !=11){
+        alert("Your Number is Not valid");
+        return;
+    }
     
+    //2: ============== Cashout Amount ==============
+    const cashoutAmountInput = document.getElementById('cashout-amount');
+    const cashoutAmount = cashoutAmountInput.value;
+    console.log(cashoutAmount);
+
+    // 3: ===== Get The Current Balance ==============
+    const balanceElement = document.getElementById('balance');
+    const balance = balanceElement.innerText;
+    console.log(balance);
+
+     // 4: ===== Calculate new Balance ==============
+     const newBalance = Number(balance) - Number(cashoutAmount) ;
+     if(newBalance < 0){
+        alert("Your Amount is not available.");
+        return;
+     }
+
+     
+
+     // 5: ===== Get The Pin Verify ==============
+     const cashoutPinInput = document.getElementById("cashout-pin")
+     const pin = cashoutPinInput.value;
+
+     if(pin === "4321"){
+        alert("Your Withdrawal Successfully Complete");
+          balanceElement.innerText = newBalance;
+        return;
+      
+     }else{
+        alert('Your Pin Number Is Not Valid');
+        return;
+     }
+console.log(newBalance);
+
+
 })
--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,15 +1,15 @@
 document.getElementById("login-btn").addEventListener('click', function(){
-    // 1: Get The Mobile Number Input
+    // 1: ============== Get The Mobile Number Input ==============
     const numberInput =document.getElementById('input-number');
     const contactNumber = numberInput.value;
     
-    // 2: Get The Pin Input
+    // 2: ============== Get The Pin Input ==============
 
     const pinInput = document.getElementById('input-pin');
     
     const checkPin = pinInput.value;
 
-    // check Mobile number and pin Number
+    // ============== check Mobile number and pin Number ==============
 
     if(contactNumber === '01978962383' && checkPin === '4321'){
         alert('Your Login Successfully complete ');
